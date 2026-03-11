6c1565ab8fdbc23680876efb77806a3d55117d5a
jananguiano <jananguiano0@gmail.com>
2026-03-11 14:14:28 +0000

    Avance1

--- cotizadorViajes.java
+++ cotizadorViajes.java
@@ -1,1 +1,24 @@
+import java.util.scanner;
+public class cotizadorviajes;
+private static final double 
+DESC_VUELO_NINO = 0.50;
+private static final double
+DESC_HOTEL_NINO = 0.30;
+private static final double
+DESC_PORC_COM = 0.12;
+public static void main (straing [ ] args ) {
+    scanner sc =new scanner (system.in);
+    System.out.print ("Cotizador de viajes express");
+    System.out.print ("Adultos:");
+    Int Adultos = sc.nextInt();
+    system.out.print ("Niños:");
+    Int Ninos = sc.next.Int();
+    System.out.print ("Noches de Hotel");
+    Int noches = sc.nextInt();
+    System.out.print ("Tarifa Base vuelo (por adulto):");
+    double tarifaVuelo = sc.nextDouble();
+    System.out.print ("Tarifa base Hotel por noche (Por Adulto)");
+    double tarifaHotel = sc.nextDouble();
+}
+
 
